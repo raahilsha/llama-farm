bd53d4573a21394e2857a83c02a798d353266bff
raahilsha <raahilsha@college.harvard.edu>
2015-04-21 17:24:28 -0400

    Llama breeding

--- LlamaFarm/src/Llama.java
+++ LlamaFarm/src/Llama.java
@@ -2,19 +2,22 @@ import java.awt.*;
 
 public class Llama
 {
-	private GameWorld world;
-	private Gene[] genome;
-	public int energy;
-	
 	// All possible states a llama can obtain
 	// Note that NIRVANA is not among these
-	public enum States { IDLE_STATE, FOOD_SEEK, EATING, AGGRESSIVE, ATTACKING, RUNNING_AWAY, RANDOM_WALK }
+	public enum State { IDLE_STATE, FOOD_SEEK, EATING, AGGRESSIVE, ATTACKING, RUNNING_AWAY, RANDOM_WALK }
+	
+	private GameWorld world;
+	private Gene[] genome;
+	private int energy;
+	
+	private State currentState;
 	
 	// Constructor for llama
 	public Llama(GameWorld gw, Gene[] g)
 	{
 		world = gw;
 		genome = g;
+		currentState = State.IDLE_STATE;
 	}
 	
 	// Renders Llama onto game panel
@@ -24,15 +27,60 @@ public class Llama
 	}
 	
 	// Breeds one llama with other
-	public Llama BreedWith(Llama other)
+	public Llama[] BreedWith(Llama other)
 	{
-		return null;
+		int offspringCount = 4;
+		int numGenes = genome.length;
+		Gene[] otherGenome = other.getGenome();
+		Llama[] offspringLlamas = new Llama[offspringCount];
+
+		Gene[][] finishedChildren = new Gene[numGenes][offspringCount];
+		Gene[][] finishedChildrenT = new Gene[offspringCount][numGenes];
+		
+		// One pass to mate all the genes individually with each other
+		for (int i = 0; i < numGenes; i++)
+		{
+			finishedChildren[i] = genome[i].CombineWith(otherGenome[i]);
+		}
+		
+		// Transpose finishedChildren to allow us to extract chains of genes
+		for (int r = 0; r < numGenes; r++)
+		{
+			for (int c = 0; c < offspringCount; c++)
+			{
+				finishedChildrenT[c][r] = finishedChildren[r][c];
+			}
+		}
+		
+		// Place all of the combined genes into new llamas
+		for (int i = 0; i < offspringCount; i++)
+		{
+			offspringLlamas[i] = new Llama(world, finishedChildrenT[i]);
+		}
+		
+		return offspringLlamas;
 	}
 	
 	// Runs the Finite State Machine of Llama 
 	public void Action(int targetX, int targetY)
 	{
-		
+		switch (currentState)
+		{
+			case IDLE_STATE:
+				break;
+			case FOOD_SEEK:
+				break;
+			case EATING:
+				break;
+			case AGGRESSIVE:
+				break;
+			case ATTACKING:
+				break;
+			case RUNNING_AWAY:
+				break;
+			case RANDOM_WALK:
+				break;
+		}
 	}
 	
 	// Accessor for genome; used when mating
